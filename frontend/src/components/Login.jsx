43cf8f4c098e11135e0b85f802deee17b5ec5d17
sashabelooov <beloovsasha23@gmail.com>
2025-09-04 21:30:19 +0500

    added navbar and fixed it

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return(
+        <div>
+            This is the login page
+        </div>
+    )
+}
+
+export default Login
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'   // createRoot import qilingan
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// root elementni olish
+const root = createRoot(document.getElementById('root'))
+
+// appni render qilish
+root.render(
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>
 )
