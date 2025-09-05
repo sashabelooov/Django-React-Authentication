e2c58410a49a0dc960dcd65b51aadcec7ae2d002
sashabelooov <beloovsasha23@gmail.com>
2025-09-05 15:59:15 +0500

    added serializers and written views function and written api endpoints

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import * 
 
-urlpatterns = [
-    
-]
+router = DefaultRouter()
+router.register('register', RegisterViewset, basename='register')
+urlpatterns = router.urls
