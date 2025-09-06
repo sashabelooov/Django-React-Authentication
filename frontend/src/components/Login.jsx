66d3bd684a4da9153de78de5246dcda30aae2e8c
sashabelooov <beloovsasha23@gmail.com>
2025-09-06 11:52:56 +0500

    updated login page it works and you can login and if you login successully you will be redirected to home page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -4,41 +4,73 @@ import MyTextField from './forms/MyTextField'
 import MyPassField from './forms/MyPassField'
 import MyButton from './forms/MyButton'
 import {Link} from 'react-router-dom'
+import {useForm} from 'react-hook-form'
+import AxiosInstance from './AxiosInstance'
+import { useNavigate } from 'react-router-dom'
+
 
 
 const Login = () =>{
+    const navigate = useNavigate()
+    const {handleSubmit, control} = useForm()
+
+    const submission = (data) => {
+        AxiosInstance.post(`login/`,{
+            email: data.email, 
+            password: data.password,
+        })
+
+        .then((response) => {
+            console.log(response)
+            localStorage.setItem("Token", response.data.token)
+            navigate(`/home`)
+        })
+        .catch(() => {
+            console.error("Error during login", error)
+        })
+    }
+    
     return(
         <div className={"myBackground"}>
+
+            <form onSubmit={handleSubmit(submission)}>
             
-            <Box className={"whiteBox"}>
+                <Box className={"whiteBox"}>
 
-                <Box className={"itemBox"}>
-                    <Box className={"title"}>Login for auth app</Box>
+                    <Box className={"itemBox"}>
+                        <Box className={"title"}>Login for auth app</Box>
+                    </Box>
+
+                    <Box className={"itemBox"}>
+                        <MyTextField 
+                        label={"Email"}
+                        name ={"email"}
+                        control={control}
+                        />
+                    </Box>
+
+                    <Box className={"itemBox"}>
+                        <MyPassField
+                        label={"Password"}
+                        name ={"password"}
+                        control={control}
+                        />
+                    </Box>
+
+                    <Box className={"itemBox"}>
+                        <MyButton 
+                            label={"Login"}
+                            type={"submit"}
+                        />
+                    </Box>
+
+                    <Box className={"itemBox"}>
+                        <Link to="/register">Don't you have an account? Register now</Link>
+                    </Box>
+
                 </Box>
 
-                <Box className={"itemBox"}>
-                    <MyTextField 
-                    lable={"Email"}
-                    />
-                </Box>
-
-                <Box className={"itemBox"}>
-                    <MyPassField
-                    lable={"Password"}
-                    />
-                </Box>
-
-                <Box className={"itemBox"}>
-                    <MyButton
-                        lable={"Login"}
-                    />
-                </Box>
-
-                <Box className={"itemBox"}>
-                    <Link to="/register">Don't you have an account? Register now</Link>
-                </Box>
-
-            </Box>
+            </form>
 
         </div>
     )
